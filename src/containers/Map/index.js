f3b57986be2d924330bdd326f8f552fcb281681a
Denis Gorobzeev <denis.gorobzeev@gmail.com>
2019-11-05 23:58:39 +0200

    change google maps

--- src/containers/Map/index.js
+++ src/containers/Map/index.js
@@ -4,7 +4,9 @@
 import React from 'react';
 
 /* MODULES */
-import { View } from 'react-native';
+import {
+  SafeAreaView, View, StyleSheet, Text
+} from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 // import MapView from 'react-native-maps';
 
@@ -20,18 +22,52 @@ import type { Node } from 'react';
 
 // type _t_props = {||};
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    paddingHorizontal: 15,
+  },
+  title: {
+    marginVertical: 15,
+  },
+  mapWrapper: {
+    height: 300,
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    // backgroundColor: 'yellow',
+    position: 'relative',
+  },
+  map: {
+    // position: 'absolute',
+    // top: 0,
+    // left: 0,
+    // right: 0,
+    // bottom: 0,
+    // flex: 1,
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
 export default (): Node => (
-  <View style={{ flex: 1, }}>
-    <MapView
-      provider={PROVIDER_GOOGLE}
-      styles={{ flex: 1 }}
-      region={{
-        latitude: 42.882004,
-        longitude: 74.582748,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-      }}
-      showsUserLocation
-    />
-  </View>
+  <SafeAreaView style={styles.container}>
+    <Text style={styles.title}>Map start</Text>
+    <View style={styles.mapWrapper}>
+      <MapView
+        styles={styles.map}
+        provider={PROVIDER_GOOGLE}
+        region={{
+          latitude: 42.882004,
+          longitude: 74.582748,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        showsUserLocation
+      />
+    </View>
+    <Text style={styles.title}>Map end</Text>
+  </SafeAreaView>
 );
